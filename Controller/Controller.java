ce5d991e9bb2d7eb82253d45c6504b921318396f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-01 22:46:18 +0300

    eight

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -44,16 +44,46 @@ public class Controller {
                 }
 
 
+            }else if(command == 3){
+
+                view.printMessage("Enter person to update: ");
+                Data newData = model.Update(view.addName(), view.addName(), view.addPosition(), view.addSalary());
+                view.printMessage("Result:  ");
+
+                if(newData != null){
+
+                    view.printData(newData);
+
+                }else {
+
+                    view.printMessage("Nothing to update");
+
+                }
+
+
+
+            } else if (command == 4) {
+
+                view.printMessage("Enter person to delete: ");
+
+                int index = model.delete(view.addName());
+
+                if(index != -1){
+
+                    view.printMessage("Deleted " + index + " element");
+
+                }else {
+
+                    view.printMessage("Nothing to delete: ");
+
+                }
+
+
+            }else {
+
+                view.printMessage("Wrong command ");
+
             }
-
-
-
-
-
-
-
-
-
 
 
         }
