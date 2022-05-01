b9cf13632087f92b30cd8b3da15432e351170d3e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-01 21:34:40 +0300

    two

---
+++ Data/Data.java
@@ -0,0 +1,20 @@
+package Data;
+
+public class Data {
+
+    String position;
+    String name;
+    Integer salary;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+}
