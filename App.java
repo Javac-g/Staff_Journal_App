ce66a81a7345771cd5a2bd413388e0862976b16e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-01 22:47:23 +0300

    nine

--- App.java
+++ App.java
@@ -1,8 +1,11 @@
+import Controller.Controller;
+
 public class App {
 
     public static void main(String...args){
 
-
+        Controller controller = new Controller();
+        controller.initController();
 
 
 
