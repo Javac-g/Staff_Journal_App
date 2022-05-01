061894f5de29c91b78e3d0bfdec7abb2dcbafe14
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-01 22:37:03 +0300

    seven

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -24,7 +24,25 @@ public class Controller {
             else if(command == 1){
 
                 view.printMessage("Add new person");
-                model.c
+                model.create(view.addName(),view.addPosition(),view.addSalary());
+                view.printMessage("Saved");
+
+            }
+            else if(command == 2){
+
+                view.printMessage("Enter person to Find: ");
+                Data data = model.read(view.addName());
+
+                if(data != null){
+
+                    view.printData(data);
+
+                }else {
+
+                    view.printMessage("Nothing to show");
+
+                }
+
 
             }
 
