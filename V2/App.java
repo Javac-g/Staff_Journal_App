31bdb032cdafb5dcab584328d0a024a7171ec2e0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-17 00:48:18 +0300

    fix done

--- V2/App.java
+++ V2/App.java
@@ -1,8 +1,11 @@
 package V2;
 
+import V2.Controller.Controller;
+
 public class App {
     public static void main(String...args){
-
+        Controller controller = new Controller();
+        controller.initController();
 
 
 
--- V2/Controller/Controller.java
+++ V2/Controller/Controller.java
@@ -10,31 +10,37 @@ public class Controller {
     View view = new View();
 
     public void initController(){
-        while (true){
+        Integer command = 0;
+        while (command  != 5){
             view.printMenu();
-            Integer command = view.readCommand();
+            command = view.readCommand();
 
             switch (command){
                 case 1:
                     view.printMesage("Enter person data: ");
                     model.create(view.addName(), view.addPosition(), view.addId(), view.addSalary());
                     view.printMesage("Person data saved");
+                    break;
                 case 2:
                     view.printMesage("Enter id person to find:");
                     Student student = model.read(view.addId());
                     if (student != null){
                         view.printData(student);
+                        break;
 
                     }else {
                         view.printMesage("Nothing to show");
+                        break;
                     }
                 case 3:
                     view.printMesage("Enter person to update");
                     Student up = model.update(view.addId(), view.addName(), view.addPosition(), view.addId(), view.addSalary());
                     if (up != null){
                         view.printData(up);
+                        break;
                     }else {
                         view.printMesage("Nothing to update");
+                        break;
                     }
                 case 4:
                     view.printMesage("Enter id to delete:");
@@ -42,16 +48,18 @@ public class Controller {
 
                     if (index != -1){
                         view.printMesage("Was deleted person № " + index);
+                        break;
 
                     }else {
                         view.printMesage("Nobody find to delete.");
+                        break;
                     }
                 case 5:
                     break;
 
 
                 default:
-                    view.printMesage("Wrong comm  and");
+                    view.printMesage("Wrong command");
             }
         }
 
