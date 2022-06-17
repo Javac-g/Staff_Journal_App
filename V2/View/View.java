ac1e88654640d045d833c3a817d13a888c2178c5
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2022-06-17 23:19:54 +0300

    Update View.java

--- V2/View/View.java
+++ V2/View/View.java
@@ -26,7 +26,7 @@ public class View {
     public Integer addId(){
         System.out.println("Enter id: ");
         Integer id = scanner.nextInt();
-        return 1111111+ id;
+        return 111111+ id;
     }
     public Integer addSalary(){
         System.out.println("Enter salary: ");
