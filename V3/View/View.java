0640dad2ac33e4082519025b678fce4508fac261
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-18 15:21:54 +0300

    message

---
+++ V3/View/View.java
@@ -0,0 +1,17 @@
+package V3.View;
+
+import java.util.Scanner;
+
+public class View {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public void printMessage(String  msg){
+
+        System.out.println(msg);
+
+    }
+
+
+
+}
